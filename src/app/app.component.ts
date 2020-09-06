2be1a603de2ddcf79fee5fb98ac2f51aa4c2b7ea
William Kolean <william.kolean@rga.com>
2020-09-06 23:29:51 +0900

    added todo and formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoList } from './todoList';
+import { TodoItem } from './todoItem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo';
+  private list = new TodoList('Bob', [
+    new TodoItem('Go for a run', true),
+    new TodoItem('Get flowers'),
+    new TodoItem('Collect tickets'),
+  ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items;
+  }
 }
---
+++ src/app/todoItem.ts
@@ -0,0 +1,9 @@
+export class TodoItem {
+  constructor(taskVal: string, completeVal: boolean = false) {
+    this.task = taskVal;
+    this.complete = completeVal;
+  }
+
+  task: string;
+  complete: boolean;
+}
