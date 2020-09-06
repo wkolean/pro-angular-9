acd0c2a069d4d1147444a0d483a9f668a18bfdf5
William Kolean <william.kolean@rga.com>
2020-09-06 23:54:39 +0900

    additional functionality to finish chapter 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,10 +19,18 @@ export class AppComponent {
   }
 
   get itemCount(): number {
-    return this.list.items.filter(item => !item.complete).length;
+    return this.items.length;
   }
 
   get items(): readonly TodoItem[] {
-    return this.list.items;
+    return this.list.items.filter(item => this.showComplete || !item.complete);
   }
+
+  addItem(newItem: string) {
+    if (newItem != '') {
+      this.list.addItem(newItem);
+    }
+  }
+
+  showComplete: boolean = false;
 }
